85443ca5b413ee2f72b8852839095e0fb6f194eb
gilbert <annatibekina3@gmail.com>
2022-08-01 15:36:49 +0300

    Initial Commit

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Анна Тибекина on 01.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
